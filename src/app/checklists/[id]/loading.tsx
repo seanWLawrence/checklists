4c9ad57df1d43c548c0708423368a52952a6a8a5
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2024-01-15 21:24:14 -0600

    improve loading skeleton

--- src/app/checklists/[id]/loading.tsx
+++ src/app/checklists/[id]/loading.tsx
@@ -6,7 +6,11 @@ export default function Loading() {
       <div className="space-y-7">
         <Heading level={1}>Checklist</Heading>
 
-        <div className="h-20 w-full max-w-prose bg-zinc-100 border-2 border-zinc-900 rounded-lg animate-pulse duration-1000"></div>
+        <div className="space-y-2 p-5 w-full max-w-prose border-2 border-zinc-900 rounded-lg animate-pulse duration-1000">
+          <div className="h-10 w-full max-w-prose bg-zinc-50 border-2 border-zinc-900 rounded-lg animate-pulse duration-1000"></div>
+          <div className="h-10 w-full max-w-prose bg-zinc-50 border-2 border-zinc-900 rounded-lg animate-pulse duration-1000"></div>
+          <div className="h-10 w-full max-w-prose bg-zinc-50 border-2 border-zinc-900 rounded-lg animate-pulse duration-1000"></div>
+        </div>
       </div>
     </div>
   );
