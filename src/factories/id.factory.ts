5d2352278bfdcf054a770bc77d7d76138936dee7
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2024-03-24 00:09:22 -0500

    convert id factory to use isomorphic UUID

--- src/factories/id.factory.ts
+++ src/factories/id.factory.ts
@@ -1,46 +1,14 @@
-import { fillArray } from "./fill-array.factory";
-import { random } from "./random";
+import { type UUID } from "crypto";
 
-const chars = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-];
+const isNode =
+  typeof process !== "undefined" &&
+  process.versions != null &&
+  process.versions.node != null;
 
-export const id = (): string => {
-  return fillArray({ length: 25, factory: () => random(chars) }).join("");
+export const id = (): UUID => {
+  if (isNode) {
+    return require("crypto").randomUUID();
+  }
+
+  return window.crypto.randomUUID() as UUID;
 };
