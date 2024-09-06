c19c9a48af4494f16c3739d7e4bef968688ddff4
Sean W. Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2024-09-06 02:39:16 -0400

    add PWA metadata to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,54 @@
-import TopNavigation from "@/components/top-navigation";
-
 import type { Metadata, Viewport } from "next";
+import type { ReactNode } from "react";
+
+import TopNavigation from "@/components/top-navigation";
 
 import "./globals.css";
 
+const APP_NAME = "SL";
+const APP_DEFAULT_TITLE = "SL";
+const APP_TITLE_TEMPLATE = "%s - App";
+const APP_DESCRIPTION = "Lifestyle app";
+
 export const metadata: Metadata = {
-  title: "Sean | App",
-  description: "Cool stuff",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
+  themeColor: "#FFFFFF"
 };
 
 export default function RootLayout({
