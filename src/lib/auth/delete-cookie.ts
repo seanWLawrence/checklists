509b44e5f02d032e55f83030480e88cc4d22471e
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2024-12-23 01:23:34 -0600

    change deleteCookie to set with expired date instead of .delete()
    .delete() works locally, but not in production

--- src/lib/auth/delete-cookie.ts
+++ src/lib/auth/delete-cookie.ts
@@ -1,11 +1,22 @@
 import { cookies } from "next/headers";
+import { getSecureCookieParams } from "./get-secure-cookie-params";
+import { isProduction } from "../environment";
+import { DOMAIN } from "../constants";
 
 export const deleteCookie = async ({
   name,
+  isProductionFn = isProduction,
+  domain = DOMAIN,
 }: {
   name: string;
+  isProductionFn?: () => boolean;
+  domain?: string;
 }): Promise<void> => {
   const cookieStore = await cookies();
 
-  cookieStore.delete(name);
+  cookieStore.set(
+    name,
+    "",
+    getSecureCookieParams({ expires: new Date(0), domain, isProductionFn }),
+  );
 };
