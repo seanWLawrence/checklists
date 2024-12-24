c54d513ff9275e74aaca233d930ac1bc733fbe9f
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2024-12-23 22:19:12 -0600

    hide time estimte if non available

--- src/components/time-estimate-badge.tsx
+++ src/components/time-estimate-badge.tsx
@@ -1,13 +1,21 @@
 import { sumTimeEstimates } from "@/app/checklists/lib/sum-time-estimates";
 import { TimeEstimate } from "@/lib/types";
 
+const NO_TIME_ESTIMATE = "0m";
+
 export const TimeEstimateBadge: React.FC<{
   timeEstimates: (TimeEstimate | undefined)[];
 }> = ({ timeEstimates }) => {
+  const sum = sumTimeEstimates(timeEstimates);
+
+  if (sum === NO_TIME_ESTIMATE) {
+    return null;
+  }
+
   return (
     <span>
       <span className="text-xs bg-zinc-200 text-zinc-900 rounded py-1 px-1.5">
-        {sumTimeEstimates(timeEstimates)}
+        {sum}
       </span>
     </span>
   );
