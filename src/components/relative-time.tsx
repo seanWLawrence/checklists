571b94ab0ec84420a890c656d6ed7bbe9414bd78
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2024-12-23 20:57:54 -0600

    italicize and shorten text on relative time

--- src/components/relative-time.tsx
+++ src/components/relative-time.tsx
@@ -2,8 +2,8 @@ import { formatRelativeTime } from "@/lib/format-relative-time";
 
 export const RelativeTime: React.FC<{ date: Date }> = ({ date }) => {
   return (
-    <span className="text-xs text-zinc-600">
-      Updated: {formatRelativeTime(date)}
+    <span className="text-xs text-zinc-600 italic">
+      {formatRelativeTime(date)}
     </span>
   );
 };
