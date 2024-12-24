dd24212c2ba38fb818603f6fdd0ed8a4eb4df598
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2024-12-23 21:57:50 -0600

    fix type errors

--- src/app/notes/[id]/prettify-note-content.ts
+++ src/app/notes/[id]/prettify-note-content.ts
@@ -1,9 +1,11 @@
 import markdownIt from "markdown-it";
 import markdownItAnchor from "markdown-it-anchor";
-import markdownItTaskLists from "markdown-it-task-lists";
 import { markdownItTable } from "markdown-it-table";
-import markdownItTableOfContents from "markdown-it-table-of-contents";
 import markdownItHighlightJs from "markdown-it-highlightjs";
+// @ts-expect-error no type definitions
+import markdownItTaskLists from "markdown-it-task-lists";
+// @ts-expect-error no type definitions
+import markdownItTableOfContents from "markdown-it-table-of-contents";
 
 export const prettifyNoteContent = ({
   content,
@@ -12,22 +14,17 @@ export const prettifyNoteContent = ({
   content: string;
   markdownItFn?: typeof markdownIt;
 }): string => {
-  try {
-    const md = markdownItFn({
-      html: false,
-      linkify: true,
-      typographer: true,
-      breaks: true,
-    })
-      .use(markdownItAnchor)
-      .use(markdownItTaskLists)
-      .use(markdownItTable)
-      .use(markdownItTableOfContents)
-      .use(markdownItHighlightJs);
+  const md = markdownItFn({
+    html: false,
+    linkify: true,
+    typographer: true,
+    breaks: true,
+  })
+    .use(markdownItAnchor)
+    .use(markdownItTaskLists)
+    .use(markdownItTable)
+    .use(markdownItTableOfContents)
+    .use(markdownItHighlightJs);
 
-    return md.render(content);
-  } catch (error) {
-    console.log(error.toString());
-    return "";
-  }
+  return md.render(content);
 };
