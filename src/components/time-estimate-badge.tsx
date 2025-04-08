b612732776501de18b31ae5a93c3665dec0d2948
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2025-04-08 10:00:36 -0500

    upgrade tailwind to v4

--- src/components/time-estimate-badge.tsx
+++ src/components/time-estimate-badge.tsx
@@ -14,7 +14,7 @@ export const TimeEstimateBadge: React.FC<{
 
   return (
     <span>
-      <span className="text-xs bg-zinc-200 text-zinc-900 rounded py-1 px-1.5">
+      <span className="text-xs bg-zinc-200 text-zinc-900 rounded-sm py-1 px-1.5">
         {sum}
       </span>
     </span>
