a807e2fb33e1b3b101ec3930b1c02144694801a5
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2025-12-21 18:28:27 -0600

    Redirect to edit current day's journal if already exists

--- src/app/journals/lib/get-today-local.ts
+++ src/app/journals/lib/get-today-local.ts
@@ -1,6 +1,9 @@
-export const getTodayLocal = () => {
+import { CreatedAtLocal } from "../journal.types";
+
+export const getTodayLocal = (): CreatedAtLocal => {
   const now = new Date();
   const month = String(now.getMonth() + 1).padStart(2, "0");
   const day = String(now.getDate()).padStart(2, "0");
-  return `${now.getFullYear()}-${month}-${day}`;
+
+  return CreatedAtLocal.unsafeDecode(`${now.getFullYear()}-${month}-${day}`);
 };
--- src/app/journals/new/page.tsx
+++ src/app/journals/new/page.tsx
@@ -1,6 +1,32 @@
 import { JournalForm } from "../components/journal-form";
+import { redirect } from "next/navigation";
+import { validateUserLoggedIn } from "@/lib/auth/validate-user-logged-in";
+import { getSingleItem } from "@/lib/db/get-single-item";
+import { getJournalKey } from "../model/get-journal.model";
+import { Journal } from "../journal.types";
+import { getTodayLocal } from "../lib/get-today-local";
+import { EitherAsync } from "purify-ts/EitherAsync";
 
-const NewJournal: React.FC = () => {
+const NewJournal: React.FC = async () => {
+  const page = await EitherAsync(async ({ fromPromise }) => {
+    const createdAtLocal = getTodayLocal();
+
+    const user = await fromPromise(validateUserLoggedIn({}));
+
+    await fromPromise(
+      getSingleItem({
+        key: getJournalKey({ user, createdAtLocal }),
+        decoder: Journal,
+      }),
+    );
+
+    return createdAtLocal;
+  }).run();
+
+  if (page.isRight()) {
+    return redirect(`/journals/${page.extract()}/edit`);
+  }
+
   return <JournalForm />;
 };
 
