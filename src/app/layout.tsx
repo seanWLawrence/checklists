4325da014967955d3186eea3666103485855a169
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2025-12-31 00:49:46 -0600

    fix(pwa): fix configuration on PWA icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,13 @@ export const metadata: Metadata = {
     template: APP_TITLE_TEMPLATE,
   },
   description: APP_DESCRIPTION,
+  icons: {
+    icon: [
+      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
