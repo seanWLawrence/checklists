2a079dfb552842f43c14a0b43d1d02a6d28eb369
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2026-01-22 12:28:34 -0600

    feat: group checklists by category in name with {{category}}/ prefix

--- src/app/checklists/page.tsx
+++ src/app/checklists/page.tsx
@@ -7,57 +7,86 @@ import { getAllChecklistsV2 } from "./model/get-all-checklists-v2.model";
 
 export const dynamic = "force-dynamic";
 
+const getCategorizedChecklists = (
+  checklists: ChecklistV2[],
+): { category: string; checklists: ChecklistV2[] }[] => {
+  const uncategorized: ChecklistV2[] = [];
+  const result: { [category: string]: ChecklistV2[] } = {};
+
+  for (const checklist of checklists) {
+    const nameSplit = checklist.name.trim().split("/");
+
+    if (nameSplit.length === 1) {
+      uncategorized.push(checklist);
+      continue;
+    }
+
+    const [category, name] = nameSplit;
+
+    if (!result[category]) {
+      result[category] = [];
+    }
+
+    result[category].push({ ...checklist, name });
+  }
+
+  if (result["Other"]) {
+    result["Other"] = uncategorized.sort((a, b) =>
+      a.name.toLowerCase().localeCompare(b.name.toLowerCase()),
+    );
+  }
+
+  return Object.entries(result)
+    .map(([category, checklists]) => {
+      return {
+        category,
+        checklists: [
+          ...checklists.sort((a, b) =>
+            a.name.toLowerCase().localeCompare(b.name.toLowerCase()),
+          ),
+        ],
+      };
+    })
+    .sort((a, b) =>
+      a.category.toLowerCase().localeCompare(b.category.toLowerCase()),
+    );
+};
+
 const ChecklistsV2: React.FC = async () => {
   const page = await EitherAsync(async ({ fromPromise }) => {
     const checklists = await fromPromise(getAllChecklistsV2());
 
-    const checklistsByFirstChar = Array.from(
-      checklists
-        .sort((a, b) =>
-          a.name.toLowerCase().localeCompare(b.name.toLowerCase()),
-        )
-        .reduce((acc, x) => {
-          const firstChar = x.name.trim().at(0)?.toLowerCase();
-
-          if (!firstChar) {
-            return acc;
-          }
-
-          const checklistsForFirstChar = acc.get(firstChar) ?? [];
-          checklistsForFirstChar.push(x);
-
-          acc.set(firstChar, checklistsForFirstChar);
-
-          return acc;
-        }, new Map<string, ChecklistV2[]>())
-        .entries(),
-    );
+    const categorizedChecklists = getCategorizedChecklists(checklists);
 
     return (
       <main>
         <section className="space-y-3">
           <Heading level={1}>Checklists</Heading>
 
-          {!checklistsByFirstChar.length && (
+          {!categorizedChecklists.length && (
             <p className="text-sm text-zinc-700">No items.</p>
           )}
 
           <div className="space-y-1">
-            {checklistsByFirstChar.map(([firstChar, checklists]) => {
+            {categorizedChecklists.map(({ category, checklists }) => {
               return (
-                <div className="flex flex-wrap" key={firstChar}>
-                  {checklists.map(({ id, name }) => {
-                    return (
-                      <LinkButton
-                        href={`/checklists/${id}`}
-                        key={id}
-                        variant="outline"
-                        className="mr-2 mb-2"
-                      >
-                        {name}
-                      </LinkButton>
-                    );
-                  })}
+                <div className="flex flex-col space-y-1" key={category}>
+                  <Heading level={3}>{category}</Heading>
+
+                  <div className="flex flex-wrap" key={category}>
+                    {checklists.map(({ id, name }) => {
+                      return (
+                        <LinkButton
+                          href={`/checklists/${id}`}
+                          key={id}
+                          variant="outline"
+                          className="mr-2 mb-2"
+                        >
+                          {name}
+                        </LinkButton>
+                      );
+                    })}
+                  </div>
                 </div>
               );
             })}
