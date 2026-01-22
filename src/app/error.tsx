2ed439f74fb4956a2f4035535a0e3943430c0d5f
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2026-01-21 23:58:55 -0600

    chore: display full error on global error handler

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,10 @@
 "use client"; // Error components must be Client Components
 
 import { Button } from "@/components/button";
+import { logger } from "@/lib/logger";
 import { useEffect } from "react";
 
-export default function Error({
+export default function GlobalError({
   error,
   reset,
 }: {
@@ -11,19 +12,24 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
-    console.error(error);
+    logger.error(error);
   }, [error]);
 
   return (
-    <div className="space-y-2">
-      <div className="flex flex-col space-y-1 bg-red-100 text-red-800 p-4 rounded">
-        <h2>Something went wrong!</h2>
-        <p>{error.message}</p>
-      </div>
+    <html>
+      <body>
+        <div className="space-y-2">
+          <div className="flex flex-col space-y-1 bg-red-100 text-red-800 p-4 rounded">
+            <h2>Something went wrong!</h2>
+            <p>{error.message}</p>
+            <p>{String(error)}</p>
+          </div>
 
-      <Button onClick={() => reset()} variant="primary">
-        Try again
-      </Button>
-    </div>
+          <Button onClick={() => reset()} variant="primary">
+            Try again
+          </Button>
+        </div>
+      </body>
+    </html>
   );
 }
