a37c06525cf4201bb3f04654a50457849c8d5146
Sean Lawrence <23564209+seanWLawrence@users.noreply.github.com>
2026-02-01 21:03:37 -0600

    fix(pull-aws-secrets): fix typo in script that prevented it from accessing aws session

--- scripts/pull-aws-secrets.sh
+++ scripts/pull-aws-secrets.sh
@@ -1,13 +1,11 @@
-npx @dotenvx/dotenvx run -f .env -- bash -c '
-  aws secretsmanager \
-    get-secret-value \
-    --secret-id "$AWS_SECRET_NAME" \
-    --query SecretString \
-    --output json \
-  | jq -r "if type == \"string\" then fromjson else . end | to_entries[] | \"\\(.key)=\\(.value)\"" \
-  | while IFS= read -r line; do
-      key="${line%%=*}"
-      value="${line#*=}"
-      npx @dotenvx/dotenvx set "$key" "$value" -f .env
-    done
-'
+aws secretsmanager \
+  get-secret-value \
+  --secret-id "$AWS_SECRET_NAME" \
+  --query SecretString \
+  --output json |
+  jq -r "if type == \"string\" then fromjson else . end | to_entries[] | \"\\(.key)=\\(.value)\"" |
+  while IFS= read -r line; do
+    key="${line%%=*}"
+    value="${line#*=}"
+    npx @dotenvx/dotenvx set "$key" "$value" -f .env
+  done
